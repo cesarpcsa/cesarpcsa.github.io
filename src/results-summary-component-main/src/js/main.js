7c9d6503236f0d678742a05abdb413590792b5e6
CesarPC21 <cesarpc880521@gmail.com>
2023-03-15 11:43:21 -0300

    update

---
+++ src/results-summary-component-main/src/js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+   a = $('#circlet').attr('width')
+    // let widthc = $('circlet').attr(widthc.width);
+
+    console.log(a);
+})
+
+
+// $(document).ready(function() {
+//     // Cambio de header
+//     $(window).on('scroll', function() {
+//         let posicion = $('#pr').offset();
+//         if ($(window).scrollTop() >= posicion.top) {
+//             $('header').removeClass('transparente');
+//         } else {
+//             $('header').addClass('transparente');
+//         }
+//     })
